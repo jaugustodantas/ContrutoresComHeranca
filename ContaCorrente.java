6fb8adb2880f901364c59fbfb74bc4af9402521e
Augusto Dantas <jnetodf@gmail.com>
2022-03-15 02:48:16 -0300

    AULA SOBRE HERANÇA E USO DE CONSTRUTORES

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,8 +1,13 @@
 public  class ContaCorrente extends Conta{
 
-public ContaCorrente(int agencia, int numero){
-    //construtor especifico da classe mãe
-    super(agencia,numero);
-}
-    
+    public ContaCorrente(int agencia, int numero){
+        //construtor especifico da classe mãe
+        super(agencia,numero);
+    }
+
+    @Override
+    public boolean saca(double valor) {
+        return super.saca(valor + 0.2) ;
+    }
+
 }
